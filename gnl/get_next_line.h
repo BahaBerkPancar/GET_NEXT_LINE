5654e1eb78c0c9c291be97dd7d561cdfb257d839
BahaBerkPancar <bahaberkpancar@gmail.com>
2024-11-08 15:40:00 +0300

    Add files via upload

---
+++ gnl/get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: MKRAForce <MKRAForce@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 16:46:31 by acerrah           #+#    #+#             */
+/*   Updated: 2024/11/08 15:24:08 by MKRAForce        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# define BUFFER 5
+
+static struct s_gnl
+{
+	char *str;
+	size_t i;
+}	t_gnl;
+
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*get_left(char *left);
+char	*get_line(char *buffer);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*get_buffer(int fd, char *left_str);
+
+#endif
